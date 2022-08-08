06754042d29a614afe8f9da23b0ee246bd998a85
ANSHxTHAKUR <sengaransh094@gmail.com>
2022-08-08 14:18:45 +0530

    second code

--- Set Matrix Zeroes.cpp
+++ Set Matrix Zeroes.cpp
@@ -1,26 +1,30 @@
-#include<iostream>
-
-using namespace std;
-
-int main()
-{
-    int i,j,n;
-    cout<<"Enter the value of n";
-    cin>>n;
-    for(i=1;i<=n;i++)
-    {
-    	for(j=1;j<=n;j++)
-    	{
-    		if(j<=n-i)
-    		{
-    			cout<<" ";
-			}
-			else
-			{
-			cout<<"*";
-    	}
-		}
-		cout<<endl;
-	}
-return 0;
-}
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        vector<int> row(n,-1);
+        vector<int> col(m,-1);
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+              row[i] = 0;
+                    col[j] = 0;
+                }
+            }
+        }
+        
+          for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(row[i]==0 || col[j]==0){
+                    matrix[i][j]=0;
+                }
+                
+            }
+        }
+    }
+};
